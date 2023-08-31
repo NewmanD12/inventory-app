83b77f8da101224222213f481bdce50babdf58b6
Dakota Newman <dakota.newman@yahoo.com>
2023-08-31 16:57:13 -0400

    enabled cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 require("dotenv").config()
 
@@ -23,6 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+app.options("*", cors());
+
 
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
